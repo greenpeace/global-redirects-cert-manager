ad2fad6410146b568b0739cd28615dd9215aa239
Jen <jhasenau@greenpeace.org>
2020-05-05 16:19:48 +1000

    specify ns for crds

--- create_crds.sh
+++ create_crds.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 #Install the CustomResourceDefinition resources first separately.
 #https://cert-manager.io/docs/installation/kubernetes/#installing-with-helm
 #Kubernetes 1.15+
-  kubectl apply --validate=false \
-	 -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.crds.yaml
+kubectl apply --validate=false -f cert-manager.crds.yaml
--- create_issuer.sh
+++ create_issuer.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 #Create ClusterIssuer to allow cert maintenance across namespaces
 # Refer to https://cert-manager.io/docs/tutorials/acme/ingress/
 
-  kubectl apply -f letsencrypt-staging.yaml
-  kubectl apply -f letsencrypt-prod.yaml
+kubectl apply -f letsencrypt-staging.yaml
+kubectl apply -f letsencrypt-prod.yaml
